c332d9042a11bc15e3f9cefe1c3b62017469b8b5
maxellmilay <milaymaxell@gmail.com>
2023-08-11 13:18:52 +0800

    [Setup][Maxell] added css import for gothic font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div></div>;
+  return <div className="font-gothic font-extralight">This is home</div>;
 };
 
 export default HomePage;
