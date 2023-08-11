3dd0a3a080909c2e91c617b0e4a1f7ae8cc39d13
maxellmilay <milaymaxell@gmail.com>
2023-08-11 17:48:45 +0800

    [UI][Maxell] made header responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,16 +10,28 @@ const Header = () => {
           width={35}
           height={35}
           alt="Logo"
+          className="hidden md:block"
         />
-        <p className="font-gotham font-bold mx-5 text-white">GAYANG MINGOR</p>
+        <p className="font-gotham font-bold md:mx-5 text-white">
+          GAYANG MINGOR
+        </p>
       </button>
-      <div className="flex items-center text-white font-gotham font-light">
+      <div className="md:flex items-center text-white font-gotham font-light hidden ">
         <button className="mr-6">About Us</button>
         <button className="mr-6">Member Directory</button>
         <button className="mr-6">Timeline</button>
         <button className="font-bold bg-mingor-yellow px-4 py-1 rounded-[70px] h-fit">
           Contact Us
         </button>
+      </div>
+      <div className="flex items-center md:hidden">
+        <Image
+          src="/images/menu-icon.png"
+          width={30}
+          height={30}
+          alt="Menu"
+          className=" h-fit"
+        />
       </div>
     </div>
   );
