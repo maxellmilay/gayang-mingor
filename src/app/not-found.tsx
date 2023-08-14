2204ed7e5690a7b9e8f9c272b6fc83e39f5f063d
maxellmilay <milaymaxell@gmail.com>
2023-08-14 16:15:39 +0800

    [Adjustment][Maxell] transferred components inside app directory

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,5 @@
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import routes from "@/enums/routes";
 import Image from "next/image";
 import Link from "next/link";
